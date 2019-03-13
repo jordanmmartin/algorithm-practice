c7334233c76778fe4ea7681a409a859f6df6b4ff
jordanmmartin <jdordan23@gmail.com>
2019-03-13 00:12:03 -0400

    added solution for reversestring exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,16 +7,9 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  // Possible Solutions
-  // 1)Return str.split('').reverse().join('')
-  // 2)let reversed = ''
-  // for(let character of str){
-  //   reversed = character + reversed
-  // }
-  //return reversed
   let arr = str.split('')
   let newStr = ''
-  for(i=arr.length - 1; i >= 0; i--) {
+  for (i=arr.length - 1; i >= 0; i--) {
     newStr = newStr + arr[i]
   }
   return newStr
