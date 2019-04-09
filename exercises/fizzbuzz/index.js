e46689385441be8c52b05f899ea6165b918818ce
jordanmmartin <jdordan23@gmail.com>
2019-04-09 17:59:13 -0400

    added recommend solution for fizzbuzz exercise that uses && to % of both 3 and 5 instead of just % 15

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,9 +13,8 @@
 //   buzz
 
 function fizzBuzz(n) {
-
 for(let i = 1; i <= n; i++){
-  if(i % 15 === 0){
+  if(i % 3 === 0 && i % 5 === 0){
     console.log('fizzbuzz')
   } else if (i % 5 === 0) {
     console.log('buzz')
@@ -28,3 +27,19 @@ for(let i = 1; i <= n; i++){
 }
 
 module.exports = fizzBuzz;
+
+
+//my intial solution
+// function fizzBuzz(n) {
+//   for(let i = 1; i <= n; i++){
+//     if(i % 15 === 0){
+//       console.log('fizzbuzz')
+//     } else if (i % 5 === 0) {
+//       console.log('buzz')
+//     } else if (i % 3 === 0) {
+//       console.log('fizz')
+//     } else {
+//       console.log(i)
+//     }
+//   }
+// }
